e07f8e7d1bc129d862c98e16954cc49143064895
tari9925 <tadas.riksas@mif.stud.vu.lt>
2025-11-30 20:42:58 +0200

    Implement change password route: integrate ChangePassword component into MainLayout

--- frontend/src/routes/auth/change-password.tsx
+++ frontend/src/routes/auth/change-password.tsx
@@ -1,9 +1,20 @@
+import { ChangePassword } from '@/components/features/auth'
+import { AppBar, MainLayout } from '@/components/layouts'
+import { AppBarData } from '@/constants'
+import { Box } from '@mui/material'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/auth/change-password')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/auth/change-password"!</div>
+    return (
+        <MainLayout>
+            <AppBar {...AppBarData} />
+            <Box sx={{ textAlign: 'center', mt: 8 }}>
+                <ChangePassword />
+            </Box>
+        </MainLayout>
+    )
 }
