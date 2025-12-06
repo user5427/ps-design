92af7a502bf6b353d0e6a0d0948b6c5c5f33a566
Dariusspr <108625236+Dariusspr@users.noreply.github.com>
2025-12-06 18:46:43 +0200

    capitalize types ands schemas

--- backend/src/routes/api/auth/response-types.ts
+++ backend/src/routes/api/auth/response-types.ts
@@ -5,11 +5,11 @@ export type {
   SuccessResponse,
 } from "../../../shared/response-types";
 export {
-  errorResponseSchema,
-  successResponseSchema,
+  ErrorResponseSchema,
+  SuccessResponseSchema,
 } from "../../../shared/response-types";
 
-export const authUserResponseSchema = z.object({
+export const AuthUserResponseSchema = z.object({
   id: z.uuid(),
   email: z.email(),
   businessId: z.uuid(),
@@ -17,14 +17,14 @@ export const authUserResponseSchema = z.object({
   isPasswordResetRequired: z.boolean(),
 });
 
-export const loginResponseSchema = authUserResponseSchema.extend({
+export const LoginResponseSchema = AuthUserResponseSchema.extend({
   accessToken: z.string(),
 });
 
-export const refreshResponseSchema = z.object({
+export const RefreshResponseSchema = z.object({
   accessToken: z.string(),
 });
 
-export type UserResponse = z.infer<typeof authUserResponseSchema>;
-export type LoginResponse = z.infer<typeof loginResponseSchema>;
-export type RefreshResponse = z.infer<typeof refreshResponseSchema>;
+export type UserResponse = z.infer<typeof AuthUserResponseSchema>;
+export type LoginResponse = z.infer<typeof LoginResponseSchema>;
+export type RefreshResponse = z.infer<typeof RefreshResponseSchema>;
--- backend/src/shared/response-types.ts
+++ backend/src/shared/response-types.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
-export const successResponseSchema = z.object({
+export const SuccessResponseSchema = z.object({
   success: z.boolean(),
 });
 
-export const errorResponseSchema = z.object({
+export const ErrorResponseSchema = z.object({
   message: z.string(),
 });
 
-export type SuccessResponse = z.infer<typeof successResponseSchema>;
-export type ErrorResponse = z.infer<typeof errorResponseSchema>;
+export type SuccessResponse = z.infer<typeof SuccessResponseSchema>;
+export type ErrorResponse = z.infer<typeof ErrorResponseSchema>;
