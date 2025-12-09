6df50cf992025d4f757b2eef13ee0f8ed25d5cd1
tari9925 <tadas.riksas@mif.stud.vu.lt>
2025-12-09 13:32:08 +0200

    permisions work, frontend not fixed yet

--- schemas/src/schemas/auth/response-types.ts
+++ schemas/src/schemas/auth/response-types.ts
@@ -4,7 +4,6 @@ export const UserResponseSchema = z.object({
   id: z.uuid(),
   email: z.email(),
   businessId: z.uuid().nullable(),
-  roleIds: z.array(z.uuid()),
   isPasswordResetRequired: z.boolean(),
 });
 
