ffba9501274785828c75d3fd4f569c63021cacc2
Dariusspr <108625236+Dariusspr@users.noreply.github.com>
2025-12-10 12:09:07 +0200

    Delete not needed sidebar links

--- frontend/src/constants/side-bar-sections.tsx
+++ frontend/src/constants/side-bar-sections.tsx
@@ -1,14 +1,11 @@
 import AssessmentIcon from "@mui/icons-material/Assessment";
 import CategoryIcon from "@mui/icons-material/Category";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import InsightsIcon from "@mui/icons-material/Insights";
 import Inventory2Icon from "@mui/icons-material/Inventory2";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import TimelineIcon from "@mui/icons-material/Timeline";
-import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import type { Section } from "@/components/layouts/side-bar/side-bar-item";
 import { URLS } from "./urls";
 
@@ -54,18 +51,10 @@ export const sidebarSections: Section[] = [
         path: URLS.MENU_CATEGORIES,
       },
       {
-        label: "Menu Items",
+        label: "Items",
         icon: <RestaurantMenuIcon />,
         path: URLS.MENU_ITEMS,
       },
-    ],
-  },
-  {
-    label: "Analytics",
-    icon: <InsightsIcon />,
-    children: [
-      { label: "Sales", icon: <TrendingUpIcon />, path: "/analytics/sales" },
-      { label: "Traffic", icon: <TimelineIcon />, path: "/analytics/traffic" },
     ],
   },
   {
